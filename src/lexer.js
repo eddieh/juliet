fd2477ec49eb8b961e78c486d5b768ae51a4f2dc
Eddie Hillenbrand <eh88@nau.edu>
2011-12-22 15:33:14 -0700

    Created LICENSE file. Removed license comment from source files.

--- src/lexer.js
+++ src/lexer.js
@@ -1,26 +1,3 @@
-/*
-  Copyright 2011 James Dean Palmer, Eddie Hillenbrand
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
-
-// Notes:
-// 03e2 is an ugly number.  Is it a malformed octal number or a
-// scientific notation double?  Java parses it as scientific but
-// I don't like it.
-// float = double
-// byte = int = long
-
 Juliet.lexer = function() {
 
   /* Pivates */
@@ -862,3 +839,10 @@ Juliet.lexer = function() {
     }
   };
 }();
+
+// Notes:
+// 03e2 is an ugly number.  Is it a malformed octal number or a
+// scientific notation double?  Java parses it as scientific but
+// I don't like it.
+// float = double
+// byte = int = long
